8799d0e724f83fd677c8e9bd98c96ad918edee64
ystrtnkl <freewillfooljoy@gmail.com>
2025-12-10 20:09:21 +0100

    0

---
+++ .docker/auto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#cd ..
+#composer create-project laravel/laravel proyecto
+#cd varwww/proyecto
+#npm install
+#composer install
+#composer update
+#composer dump-autoload
+#cd ../..
+#php artisan serve
+docker compose down
+docker compose up -d
+firefox http://localhost:8081/inicio &
+docker exec -it crud-votaciones-mariadb-cont-1 /bin/chmod -R 777 /var/lib/mysql
+docker exec -it apache_php-cont /bin/chmod -R 777 /var/www/html
+docker exec -it apache_php-cont bash -c 'while true; do chmod -R 777 /var/www/html; sleep 2; done' #&
+
+
+
+#docker exec -it apache_php-cont /var/www/html/perms.sh
+
